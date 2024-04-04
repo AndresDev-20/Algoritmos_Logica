877483b98dfd35346de486fd4f69056b5d86e1cf
Yeison Andrés Marroquín Bernal <121591987+yeisonmarroquin887@users.noreply.github.com>
2024-04-04 16:32:17 -0500

    Logica

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//su trabajo es hacer la multiplicacion y retoinar el resultado
+//Tu magia Aqui...
+const n1 = 12,
+         n2 = 24;
+ function multiplicacion(num1, num2){
+	
+ }
+ console.log(multiplicacion(n1, n2))
