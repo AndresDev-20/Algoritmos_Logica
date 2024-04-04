387cf0d9bfb52d88fa46556b4b565a3992b5ecc5
Yeison Andrés Marroquín Bernal <121591987+yeisonmarroquin887@users.noreply.github.com>
2024-04-04 17:03:58 -0500

    Logica

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 //su trabajo es hacer la multiplicacion y retoinar el resultado
 //Tu magia Aqui...
-const n1 = 12,
+const n1 = 70,
          n2 = 24;
+
+
  function multiplicacion(num1, num2){
-	
+	let res = num1 * num2;
+	return res
  }
  console.log(multiplicacion(n1, n2))
+
+ /* 
+ El dia de hoy hay una fiesta en un antro de bogota, pero los usuarios necesitan que tu coloques 
+ tu edad en un programa y si tienes mas o 18 años puedes pasar de lo contrario el programana te dira vete a dormir 
+ usa una funcion para eso, suerte colega eres un crack
+ */
+/*
+Ejemplo: 
+18 = puedes pasar 
+15 = vete a dormir 
+50 = lo siento estas muy viejo para esto vete a dormir 
+ * 
+ */
+//Tu magia aqui...
