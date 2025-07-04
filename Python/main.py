dd028cc3985a6903e9e6e7e222c4d6a0189ad542
AndresDev20 <andresmarroquin887@gmail.com>
2025-07-04 08:50:42 -0500

    Getting started in Python

---
+++ Python/main.py
@@ -0,0 +1,24 @@
+# Primer ejercicio
+# Crea una funcion que encuentre los indices de los dos numero que sumen un numero concreto
+def two_sum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return (i, j)
+    return None
+
+print(two_sum([2, 7, 11, 15], 9))  # (0, 1)
+print(two_sum([2, 7, 11, 15], 90))  # None
+
+
+
+# Segundo ejercicio
+# Crea una funcion que verifique si un numero es palindromo con true o false
+def is_palindrome(num):
+    s = str(num)
+    return s == s[::-1]
+
+print(is_palindrome(121))  # True
+print(is_palindrome(123))  # False
+
+# Tercer ejercicio
