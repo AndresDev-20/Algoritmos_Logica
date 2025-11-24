12bc0081ae3867e8caf7b7ecbfb51d4c657c87f8
AndresDev20 <andresmarroquin887@gmail.com>
2025-11-24 18:41:46 -0500

    Logic

--- Python/main.py
+++ Python/main.py
@@ -53,5 +53,7 @@ suma(44, 66)
  # - Los polígonos soportados serán Triángulo, Cuadrado y Rectángulo.
  # - Imprime el cálculo del área de un polígono de cada tipo.
  
-def polygon_area():
-    
+def polygon_area(poligono):
+    print(0)
+
+polygon_area()
