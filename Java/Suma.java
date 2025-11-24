10e8b68f8b5a0667b4d51f6097892146b7ee3baa
AndresDev20 <andresmarroquin887@gmail.com>
2025-11-24 18:19:56 -0500

    Logic

---
+++ Java/Suma.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class Suma {
+    
+    public static void main(String[] args) {
+       System.out.println(suma(33, 45));
+    }
+
+    public static int suma(int n1, int n2) {
+        return n1 + n2;
+    }
+}
--- Python/main.py
+++ Python/main.py
@@ -38,3 +38,19 @@ def name():
 
 # You can call the 'name' function to run fizz_buzz:
 # name()
+
+# Crea una funcion que sume dos numeros
+def suma(n1, n2):
+    print(n1 + n2)
+
+suma(44, 66)
+
+
+
+ # Crea una única función (importante que sólo sea una) que sea capaz
+ # de calcular y retornar el área de un polígono.
+ # - La función recibirá por parámetro sólo UN polígono a la vez.
+ # - Los polígonos soportados serán Triángulo, Cuadrado y Rectángulo.
+ # - Imprime el cálculo del área de un polígono de cada tipo.
+ 
+def polygon_area
