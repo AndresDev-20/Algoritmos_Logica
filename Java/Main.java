f001cb397b747534c5469febe54d051230371f39
AndresDev20 <andresmarroquin887@gmail.com>
2025-11-30 18:10:50 -0500

    Logic

--- Java/Main.java
+++ Java/Main.java
@@ -3,40 +3,7 @@ package Java;
 public class Main {
 
     public static void main(String[] args) {
-        {
-            /*PRIMER EJERCICIO:
-             * - Crea un comentario en el código y coloca la URL del sitio web oficial del
-             * lenguaje de programación que has seleccionado.
-             * - Representa las diferentes sintaxis que existen de crear comentarios
-             * en el lenguaje (en una línea, varias...).
-             * - Crea una variable (y una constante si el lenguaje lo soporta).
-             * - Crea variables representando todos los tipos de datos primitivos
-             * del lenguaje (cadenas de texto, enteros, booleanos...).
-             * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
-             */
-
-            // https://www.w3schools.com/java/
-            /*
-             * Iniciamos nuestro camino por el mundo de la logica, resolviendo los problemas
-             * desde un nivel
-             * principiante a un nivel muy elevado, aspiro dominar la logica y resolver todo
-             * problema que se
-             * me cruce en mi camino de ingeniero de software.
-             */
-            final String name = "Andres.dev";
-            System.out.println(name);
-            byte edad = 25;
-            short cantidad = 1000;
-            int poblacion = 500000;
-            long distancia = 1200000L;
-            float precio = 19.99f;
-            double pi = 3.14159265359;
-            char letra = 'A';
-            boolean verdadero = true;
-            boolean falso = false;
-            System.out.println("Hola, Java");
-
-        }
-
+        //PRIMER EJERCICIO:
     }
+
 }
