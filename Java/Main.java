6a1a423cc4a5da2143b8328960bbbc4840e1d982
AndresDev20 <andresmarroquin887@gmail.com>
2025-12-02 15:08:26 -0500

    Logic

--- Java/Main.java
+++ Java/Main.java
@@ -1,23 +1,14 @@
 package Java;
 
-import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        //PRIMER EJERCICIO:
-        isValid("()[]{}");
+        
     }
 
     // Funciones
     // primer ejercicio
-    public static boolean isValid(String s) {
-        ArrayList<String> str = new ArrayList();
-        str.add(s);
-        for (String element : str) {
-            System.out.println(element);
-        }
-        return true;
-    }
+    
 
 }
