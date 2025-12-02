1e530a2d350289028a60f07fa44611b3ebbfd3cd
AndresDev20 <andresmarroquin887@gmail.com>
2025-12-02 15:46:52 -0500

    Logic

--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,6 @@
 package Java;
 
+import java.util.ArrayList;
 
 public class Main {
 
@@ -7,14 +8,17 @@ public class Main {
         //1. Contar cuántos números pares hay en una lista
         // Dada una lista/arreglo de números, devuelve cuántos son pares.
         // Ejemplo: [1,2,4,7] → 2
-        int[] array = {2,5,6,7};
+        int[] array = {2,3,5,6,8};
+        ArrayList<Integer> pares = new ArrayList<>();
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 == 0) {
-                System.out.println("1" + array[i]);
+                pares.add(array[i]);
+                System.out.println("1: " + array[i]);
             } else {
-                System.out.println("2" + array[i]);
+                System.out.println("2: " + array[i]);
             }
         }
+        System.out.print(pares);
     }
 
     // Funciones
