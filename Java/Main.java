1ad203bf70624ba6ab6f824a88a5c5598973f6db
AndresDev20 <andresmarroquin887@gmail.com>
2025-12-02 15:32:13 -0500

    Logic

--- Java/Main.java
+++ Java/Main.java
@@ -4,6 +4,9 @@ package Java;
 public class Main {
 
     public static void main(String[] args) {
+        //1. Contar cuántos números pares hay en una lista
+        // Dada una lista/arreglo de números, devuelve cuántos son pares.
+        // Ejemplo: [1,2,4,7] → 2
         
     }
 
