ce40f2f12d92dadb22ef216b93e4e2874f1c21b8
AndresDev20 <andresmarroquin887@gmail.com>
2025-12-02 15:33:53 -0500

    Logic

--- Java/Main.java
+++ Java/Main.java
@@ -7,11 +7,10 @@ public class Main {
         //1. Contar cuántos números pares hay en una lista
         // Dada una lista/arreglo de números, devuelve cuántos son pares.
         // Ejemplo: [1,2,4,7] → 2
-        
+        int[] array = {2,5,6,7};
+
     }
 
     // Funciones
-    // primer ejercicio
     
-
 }
