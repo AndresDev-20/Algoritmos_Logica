72a157608557307ede8000c683447c796180d940
AndresDev20 <andresmarroquin887@gmail.com>
2025-12-03 14:00:31 -0500

    Logic

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -77,3 +77,8 @@ function name() {
 // 1. Contar cuántos números pares hay en una lista
 // Dada una lista/arreglo de números, devuelve cuántos son pares.
 // - Ejemplo: [1,2,4,7] → 2
+function ParesCount(array) {
+    
+}
+
+console.log(ParesCount([1,2,3,4,5,6]))
