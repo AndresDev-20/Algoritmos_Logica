04930603a396284a1a89c201dba8d675ead4a9ea
AndresDev20 <andresmarroquin887@gmail.com>
2025-12-14 16:19:55 -0500

    Logic

--- Python/main.py
+++ Python/main.py
@@ -1,6 +1,6 @@
 # 1)---------------------------------------------------------------------------------------
 # Crea una funcion que encuentre los indices de los dos numero que sumen un numero concreto
 def two_sum(nums, target):
-    for i, num in nums:
-        print
+    for i, num in enumerate(nums):
+        print(i)
 print(two_sum([2, 7, 11, 15], 9))
