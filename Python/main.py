1897966410d45720e3d36ebd97b3608566884abd
AndresDev20 <andresmarroquin887@gmail.com>
2025-12-14 16:04:46 -0500

    Logic

---
+++ Python/main.py
@@ -0,0 +1,5 @@
+# 1)---------------------------------------------------------------------------------------
+# Crea una funcion que encuentre los indices de los dos numero que sumen un numero concreto
+def two_sum(){
+    
+}
