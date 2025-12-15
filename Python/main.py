cac5e2baeb1bef3a15c4b87ecedd2ae172c2f42d
AndresDev20 <andresmarroquin887@gmail.com>
2025-12-15 16:35:09 -0500

    Logic

--- Python/main.py
+++ Python/main.py
@@ -70,6 +70,7 @@ polygon_area("CUADRADO")
 
 
 
+# 5)----------------------------------------------------------------------------------------
  # Crea un programa que invierta el orden de una cadena de texto
  # sin usar funciones propias del lenguaje que lo hagan de forma automática.
  # - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
