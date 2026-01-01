0b1937b159a55be659a6ed49bcb431277802a0ce
AndresDev20 <andresmarroquin887@gmail.com>
2026-01-01 18:32:28 -0500

    Logic

--- Java/Main.java
+++ Java/Main.java
@@ -273,7 +273,9 @@ public class Main {
     // 12)---------------------------------------------------------------------------------------------
     public static int sumLength(String text) {
         int res = 0;
-        for(int i = 0; i < text.length(); )
+        for(int i = 0; i < text.length(); i++) {
+            
+        }
         return 0;
     }
 }
